415d46ff6659d6b24cf3ea69d004e6794bdd8e98
Alan Thompson <cloojure@gmail.com>
2017-11-19 19:08:05 -0800

    misc

--- hello-world/build.boot
+++ hello-world/build.boot
@@ -3,5 +3,8 @@
   :dependencies '[[io.pedestal/pedestal.service "0.5.1"]
                   [io.pedestal/pedestal.route "0.5.1"]
                   [io.pedestal/pedestal.jetty "0.5.1"]
-                  [org.slf4j/slf4j-simple "1.7.21"]]
+                  [org.slf4j/slf4j-simple "1.7.21"]
+                  [tupelo "0.9.64"]
+                  ]
+
   )
--- hello-world/src/hello.clj
+++ hello-world/src/hello.clj
@@ -1,9 +1,31 @@
 (ns hello                                        
-  (:require [io.pedestal.http :as http]          
-            [io.pedestal.http.route :as route]))
+  (:require
+   ;[tupelo :as t]
+    [io.pedestal.http :as http]
+    [io.pedestal.http.route :as route]))
+;(t/refer-tupelo)
 
-(defn respond-hello [request]
-  {:status 200 :body "Hello, world!"})
+(def unmentionables #{"YHWH" "Voldemort" "Mxyzptlk" "Rumplestiltskin" "曹操"})
+
+(defn ok [body]
+  {:status 200 :body body})
+
+(defn not-found []
+  {:status 404 :body "Not Found \n"})
+
+(defn greeting-for [name]
+  (cond
+    (unmentionables name) nil
+    (empty? name) "Hello World!\n"
+    :else (str "Hello, " name \newline)))
+
+(defn respond-hello
+  [request]
+  (let [name (get-in request [:query-params :name])
+        resp (greeting-for name)]
+    (if resp
+      (ok resp)
+      (not-found))))
 
 (def routes
   (route/expand-routes
